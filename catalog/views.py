6d6f7850a5c6cb82043f73e5ac2ccc7e28a34179
Tigran Saatchyan TSA <mr.saatchyan@yandex.com>
2023-08-19 12:26:34 +0400

    [HOMEWORK] completed Homework

--- catalog/views.py
+++ catalog/views.py
@@ -1,37 +1,33 @@
 """Views module"""
 from django.core.handlers.wsgi import WSGIRequest
+from django.forms import inlineformset_factory
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.utils.text import slugify
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, \
     UpdateView, DeleteView
+from pytils.translit import slugify
 
-from catalog.models import Product, Contact, Category, Posts
+from catalog.forms import ProductForm, ProductVersionForm, PostForm, \
+    ProductVersionFormset
+from catalog.models import Product, Contact, Category, Posts, ProductVersion
 from catalog.utils import send_mail
 
 
-class CategoryListView(ListView):
-    model = Category
-    template_name = 'catalog/category_list.html'
-    context_object_name = 'categories'
+class PostSlugifyMixin:
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        slug = slugify(post.title)
+        post_objects = Posts.objects
+        if post_objects.filter(slug=slug).exists():
+            count = 1
+            while post_objects.filter(slug=f'{slug}-{count}').exists():
+                count += 1
+            slug = f'{slug}-{count}'
 
-
-class ProductListView(ListView):
-    model = Product
-    template_name = 'catalog/product_list.html'
-    context_object_name = 'products'
-    paginate_by = 4
-    ordering = ['-creation_date']
-    queryset = Product.objects.filter(
-        is_active=True
-    )
-
-
-class ProductDetailedView(DetailView):
-    model = Product
-    template_name = 'catalog/product_detail.html'
-    context_object_name = 'product'
+        post.slug = slug
+        post.save()
+        return super().form_valid(form)
 
 
 class MainListView(ListView):
@@ -77,6 +73,12 @@ def contacts(request: WSGIRequest) -> HttpResponse:
     return render(request, 'catalog/contacts.html', context)
 
 
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'catalog/category_list.html'
+    context_object_name = 'categories'
+
+
 class CategoryProductsListView(ListView):
     model = Product
     template_name = 'catalog/product_list.html'
@@ -101,22 +103,99 @@ class CategoryProductsListView(ListView):
         return context
 
 
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'products'
+    paginate_by = 4
+    ordering = ['-creation_date']
+    queryset = Product.objects.filter(
+        is_active=True
+    )
+
+
+class ProductDetailedView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
+
+
 class ProductCreateView(CreateView):
     model = Product
-    fields = (
-        'name', 'description', 'image',
-        'category', 'purchase_price', 'is_active'
-    )
+    form_class = ProductForm
     success_url = reverse_lazy('catalog:product_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(
+            Product,
+            ProductVersion,
+            form=ProductVersionForm,
+            extra=1
+        )
+
+        if self.request.method == 'POST':
+            context['version_formset'] = VersionFormset(
+                self.request.POST
+            )
+        else:
+            context['version_formset'] = VersionFormset()
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        version_formset = context['version_formset']
+
+        self.object = form.save()
+        if version_formset.is_valid():
+            version_formset.instance = self.object
+            version_formset.save()
+
+        return super().form_valid(form)
 
 
 class ProductUpdateView(UpdateView):
     model = Product
-    fields = (
-        'name', 'description', 'image',
-        'category', 'purchase_price', 'is_active'
-    )
+    form_class = ProductForm
     success_url = reverse_lazy('catalog:product_list')
+
+    def get_success_url(self):
+        return reverse('catalog:product_list')
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(
+            Product,
+            ProductVersion,
+            form=ProductVersionForm,
+            formset=ProductVersionFormset,
+            extra=1
+        )
+
+        if self.request.method == 'POST':
+            version_formset = VersionFormset(
+                self.request.POST,
+                instance=self.object
+            )
+        else:
+            version_formset = VersionFormset(instance=self.object)
+
+        context_data['version_formset'] = version_formset
+
+        return context_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        version_formset = context_data['version_formset']
+
+        self.object = form.save()
+        if version_formset.is_valid():
+            version_formset.instance = self.object
+            version_formset.save()
+        else:
+            return self.form_invalid(form)
+
+        return super().form_valid(form)
 
 
 class ProductDeleteView(DeleteView):
@@ -124,50 +203,6 @@ class ProductDeleteView(DeleteView):
     context_object_name = 'product'
     success_url = reverse_lazy('catalog:product_list')
 
-
-# Добавление продукта через форму из шаблона product_form_bak.html
-# def get_current_autoincrement_value(table_name, column_name='id'):
-#     sequence_name = f"{table_name}_{column_name}_seq"
-#
-#     with connection.cursor() as cursor:
-#         cursor.execute(f"SELECT nextval('{sequence_name}');")
-#         cursor.execute(f"SELECT currval('{sequence_name}');")
-#         result = cursor.fetchone()
-#
-#     return result[0] if result else None
-
-
-# def new_product(request: WSGIRequest) -> HttpResponse:
-#     category_list = Category.objects.all()
-#     category_mapping = {}
-#     current_id = get_current_autoincrement_value('catalog_product')
-#
-#     if request.POST:
-#         name = request.POST.get('name')
-#         description = request.POST.get('description')
-#         image = request.FILES['image']
-#         category_id = request.POST.get('category')
-#         purchase_price = request.POST.get('price')
-#         is_active = request.POST.get('is_active')
-#
-#         for category in category_list:
-#             category_mapping[category.id] = category
-#
-#         Product.objects.create(
-#             id=current_id,
-#             name=name,
-#             description=description,
-#             image=image,
-#             category=Category.objects.get(id=category_id),
-#             purchase_price=purchase_price,
-#             is_active=is_active
-#         )
-#
-#     context = {
-#         "categories": category_list
-#     }
-#
-#     return render(request, 'catalog/product_form.html', context)
 
 class PostListView(ListView):
     model = Posts
@@ -209,25 +244,11 @@ class PostDetailView(DetailView):
         return self.object
 
 
-class PostCreateView(CreateView):
+class PostCreateView(PostSlugifyMixin, CreateView):
     model = Posts
     template_name = 'catalog/posts_form.html'
-    fields = ('title', 'content', 'image', 'is_published')
     slug_url_kwarg = 'slug'
-
-    def form_valid(self, form):
-        post = form.save(commit=False)
-        slug = slugify(post.title)
-        post_objects = Posts.objects
-        if post_objects.filter(slug=slug).exists():
-            count = 1
-            while post_objects.filter(slug=f'{slug}-{count}').exists():
-                count += 1
-            slug = f'{slug}-{count}'
-
-        post.slug = slug
-        post.save()
-        return super().form_valid(form)
+    form_class = PostForm
 
     def get_success_url(self):
         return reverse_lazy(
@@ -236,25 +257,12 @@ class PostCreateView(CreateView):
         )
 
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(PostSlugifyMixin, UpdateView):
     model = Posts
     template_name = 'catalog/posts_form.html'
-    fields = ('title', 'content', 'image', 'is_published')
     slug_url_kwarg = 'slug'
     context_object_name = 'post'
-
-    def form_valid(self, form):
-        post = form.save(commit=False)
-        slug = slugify(post.title)
-        post_objects = Posts.objects
-        if post_objects.filter(slug=slug).exists():
-            count = 1
-            while post_objects.filter(slug=f'{slug}-{count}').exists():
-                count += 1
-            slug = f'{slug}-{count}'
-        post.slug = slug
-        post.save()
-        return super().form_valid(form)
+    form_class = PostForm
 
     def get_success_url(self):
         return reverse_lazy(
@@ -266,5 +274,5 @@ class PostUpdateView(UpdateView):
 class PostDeleteView(DeleteView):
     model = Posts
     context_object_name = 'post'
-    success_url = reverse_lazy('catalog:post_list')
     slug_url_kwarg = 'slug'
+    success_url = reverse_lazy('catalog:post_list')
